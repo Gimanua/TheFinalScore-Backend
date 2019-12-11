f261936c9df396820ba156e99c5ea6e9d55b0eed
Adrian Klasson <31548090+Gimanua@users.noreply.github.com>
2019-12-11 13:46:17 +0100

    Search movies

--- src/main/java/nu/te4/thefinalscore/backend/beans/MovieBean.java
+++ src/main/java/nu/te4/thefinalscore/backend/beans/MovieBean.java
@@ -1,19 +1,33 @@
 package nu.te4.thefinalscore.backend.beans;
 
-import nu.te4.thefinalscore.backend.entities.Movie;
+import kong.unirest.HttpResponse;
+import kong.unirest.JsonNode;
+import kong.unirest.Unirest;
 
 import javax.ejb.Stateless;
 import javax.ws.rs.core.Response;
-import java.util.ArrayList;
-import java.util.List;
 
 @Stateless
 public class MovieBean {
 
-    public Response getMovies(String query){
-        Response.Status status = Response.Status.OK;
-        List<Movie> movies = new ArrayList();
+    private String apiKey = "be96f5a969c038665fdfab7c659f24bf";
 
-        return Response.status(status).entity(movies).build();
+    public Response getMovies(String query){
+
+        Response.Status status = Response.Status.OK;
+        HttpResponse<JsonNode> response =
+                Unirest.get("https://api.themoviedb.org/3/search/movie?include_adult=false&page=1&query={query}&language=en-US&api_key={apiKey}")
+                .routeParam("query", query)
+                .routeParam("apiKey", apiKey)
+                .asJson();
+
+        if(!response.isSuccess()){
+            System.out.println("Something went wrong");
+            status = Response.Status.BAD_REQUEST;
+        }
+        else
+            System.out.println(response.getBody().getObject());
+
+        return Response.status(status).entity(response.getBody().getObject()).build();
     }
 }
